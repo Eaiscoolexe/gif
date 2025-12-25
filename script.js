510074637f21b58b8261083fe4ec85202d76dd6f
Eaiscoolexe <154662351+Eaiscoolexe@users.noreply.github.com>
2025-12-25 19:36:07 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("btn");
-const gif = document.getElementById("myGif");
-
-button.addEventListener("click", () => {
-  alert("GIF is fun!");
-  gif.style.border = "2px solid red"; // just a fun effect
-});
